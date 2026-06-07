55a5553db7724e59cf9cb42dd1fc5aa41c61952f
Surdhan <prssuriya2002@gmail.com>
2026-06-07 10:29:34 +0530

    Fixed GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MY_profile/',   // MUST MATCH YOUR REPO NAME
+  base: '/suriyamoorthy-portfolio/',
 })
